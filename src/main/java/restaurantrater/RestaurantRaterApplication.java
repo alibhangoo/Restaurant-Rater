a8f7e638369e17485155c8137ca9fe85c367b4e8
saeedosman <saeedosman@users.noreply.github.com>
2018-03-24 12:07:53 -0400

    Generated spring boot project

---
+++ src/main/java/restaurantrater/RestaurantRaterApplication.java
@@ -0,0 +1,12 @@
+package restaurantrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantRaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantRaterApplication.class, args);
+	}
+}
