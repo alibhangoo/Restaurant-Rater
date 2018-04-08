2fe64a1f62bd63177860e9594c58fa552cf2d384
haiderb <gaylibanks@gmail.com>
2018-04-08 13:37:19 -0400

    Add homepage component

--- restaurant-frontend/src/app/app.module.ts
+++ restaurant-frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
